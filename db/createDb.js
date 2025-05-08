80cf0cdf998c93d9b512a95036c8204f408be3d1
Hangold0078 <eaa24hgo@students.eaaa.dk>
2025-05-08 16:55:58 +0200

    change check constraint

--- db/createDb.js
+++ db/createDb.js
@@ -28,7 +28,7 @@ await db.query(`
 await db.query(`
     create table trade(
         country     varchar(128),
-        type        varchar(20) check (type in ('import quantity','export quantity')),
+        type        varchar(20) check (type in ('Import','Export')),
         year        integer not null,
         amount      decimal(10,2)
     )
