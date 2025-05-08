4ac839ffc4d0a51e095aa220aeffed2805fc4cc8
mohamedfarah711 <eaa24mofh@students.eaaa.dk>
2025-05-08 12:44:07 +0200

    added quiz questions to questions.js

--- Website/questions.js
+++ Website/questions.js
@@ -1,17 +1,41 @@
 // Liste med 5 quiz-spørgsmål
 const questions = [
     {
-      question: "",
+      question: "Which of these countries produces the most eggs?",
       answers: [
-        { text: "", correct: true },
-        { text: "", correct: false }
+        { text: "Japan", correct: true },
+        { text: "India", correct: false }
       ]
     },
     {
-      question: "",
+      question: "How many eggs does an average Danish hen produce per year?",
       answers: [
-        { text: "", correct: true },
-        { text: "", correct: false }
+        { text: "300", correct: true },
+        { text: "150", correct: false }
+      ]
+    },
+
+    {
+      question: "What type of egg production is most common in Denmark?",
+      answers: [
+        { text: "organic eggs", correct: false },
+        { text: "barnic eggs", correct: true }
+      ]
+    },
+
+    {
+      question: "Which country imports the most eggs from the EU?",
+      answers: [
+        { text: "Saudi Arabia", correct: true },
+        { text: "Norway", correct: false }
+      ]
+    },
+
+    {
+      question: "Why are eggs rarely exported over very long distances?",
+      answers: [
+        { text: "Because eggs are too heavy and expensive to transport", correct: false },
+        { text: "Because they are fragile and have a short shelf life", correct: true }
       ]
     },
     
