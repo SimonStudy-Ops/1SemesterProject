0636ac106980304c55b264b76b0aaac91bac75c9
SimonStudy-Ops <eaa24smlk@students.eaaa.dk>
2025-05-08 11:48:26 +0200

    Start of questions.js and made quiz style sheat

---
+++ Website/questions.js
@@ -0,0 +1,18 @@
+// Liste med 5 quiz-spørgsmål
+const questions = [
+    {
+      question: "",
+      answers: [
+        { text: "", correct: true },
+        { text: "", correct: false }
+      ]
+    },
+    {
+      question: "",
+      answers: [
+        { text: "", correct: true },
+        { text: "", correct: false }
+      ]
+    },
+    
+  ];
