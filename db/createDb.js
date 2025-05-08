87801a7dc5d261f8fded3c74f9b09e4554e5aa0d
Hangold0078 <eaa24hgo@students.eaaa.dk>
2025-05-08 11:45:02 +0200

    setup Dbscript

---
+++ db/createDb.js
@@ -0,0 +1,25 @@
+import pg from 'pg';
+import dotenv from 'dotenv';
+import { upload } from 'pg-upload';
+
+dotenv.config();
+console.log('Connecting to database', process.env.PG_DATABASE);
+const db = new pg.Pool({
+    host:     process.env.PG_HOST,
+    port:     parseInt(process.env.PG_PORT),
+    database: process.env.PG_DATABASE,
+    user:     process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    ssl:      { rejectUnauthorized: false },
+});
+const dbResult = await db.query('select now()');
+console.log('Database connection established on', dbResult.rows[0].now);
+
+console.log('Dropping table if they already exist....')
+//-- Slet eksisterende tabeller, hvis de findes
+await db.query(`
+        
+        drop table if exists import;
+        drop table if exists export;
+        
+    `)
